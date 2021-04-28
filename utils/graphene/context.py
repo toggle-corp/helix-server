125f18658a06bde1f02d4419496a771e05b9640e
annshress <ann.shress@gmail.com>
2021-04-28 14:47:29 +0545

    Add dataloader for review count in crisis

--- utils/graphene/context.py
+++ utils/graphene/context.py
@@ -1,6 +1,7 @@
 from django.utils.functional import cached_property
 
 from apps.event.dataloaders import EventReviewCountLoader
+from apps.crisis.dataloaders import CrisisReviewCountLoader
 
 
 class GQLContext:
@@ -14,3 +15,7 @@ class GQLContext:
     @cached_property
     def event_event_review_count_dataloader(self):
         return EventReviewCountLoader()
+
+    @cached_property
+    def crisis_crisis_review_count_dataloader(self):
+        return CrisisReviewCountLoader()
